3d933907259e7fc98e339c210ae3acaaca444286
Chenlin1984 <cheng10021@gmail.com>
2025-04-01 02:20:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-# main.py
 from dashboard import app
 
-# ✅ Render 會從這裡啟動 Web Service
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(__import__('os').environ.get("PORT", 10000)))
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
